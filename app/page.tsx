71ebda0f2dc1384d7f15e1601787084704d9a3df
m1racl3 <eralmendral@gmail.com>
2023-07-28 07:11:14 +0800

    car details

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,49 @@
 import Image from 'next/image'
-import { Hero } from '@/components'
+import { Hero, SearchBar, CustomFilter, CarCard } from '@/components'
+import { fetchCars } from '../utils'
 
-export default function Home() {
-  return (
-    <main className="">
-      <Hero/>
-    </main>
-  )
+export default async function Home() {
+    const allCars = await fetchCars()
+
+    const isDataEmpty = !allCars || !Array.isArray(allCars) || !allCars.length
+
+    return (
+        <main className="">
+            <Hero />
+
+            <div className='mt-12 padding-x padding-y max-width' id="discover">
+                <div className="home__text-container">
+                    <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+                    <p>Explore the cars you might like</p>
+                </div>
+
+                <div className='home__filters'>
+                    <SearchBar />
+
+
+
+                    <div className="home__filter-container">
+                        <CustomFilter title="fuel" />
+                        <CustomFilter title="year" />
+                    </div>
+                </div>
+
+                {!isDataEmpty ? (
+                    <section>
+                        <div className="home__cars-wrapper">
+                            {allCars.map(car => (
+                                <CarCard car={car} />
+                            ))}
+                        </div>
+                    </section>) :
+                    (
+                        <div className='home__error-container'>
+                            <h2 className="text-black text-xl font-bold">No Results</h2>
+                            <p>{allCars?.message}</p>
+                        </div>
+                    )
+                }
+            </div>
+        </main>
+    )
 }
